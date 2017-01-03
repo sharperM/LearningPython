f4d78446f4c59b3056440b0c15a98d0fb8f3b0e3
sharperM <mshaopei@gmail.com>
2017-01-03 11:17:15 +0800

    提交

--- renameFile.py
+++ renameFile.py
@@ -2,7 +2,7 @@
 import os
 import sys
 # import chardet
-path = 'dir1\\'
+path = 'C:\\Users\\admin\\Desktop\\日志'.decode("utf-8").encode('gbk')
 reload(sys)
 sys.setdefaultencoding('UTF-8')
 filename = {"11212":"11111","新建文本文档 - 副本".decode("utf-8").encode("gbk"):1}
@@ -28,3 +28,9 @@ for x in filename:
 	# print unicode("拉萨的肌肤","")
 
 	print x.decode('gbk').encode("utf-8")
+
+print(hex(hash("123")))
+
+with open('C:\\Users\\admin\\Desktop\\日志\\测试 “登录服务器” .txt'.decode("utf-8").encode('gbk')) as fp:
+    for line in iter(fp.readline, ''):
+        print(line.decode("gbk").encode('utf-8'))
