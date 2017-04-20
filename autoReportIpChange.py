1d273d2a97e50d065397e237b1131a8ec3ad9b2f
sharperM <mshaopei@gmail.com>
2017-04-20 10:29:43 +0800

    提交

---
+++ autoReportIpChange.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding=utf-8
+from threading import Timer,Thread,Event
+from requests import get
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+
+# 第三方 SMTP 服务
+mail_host="smtp.qq.com"  #设置服务器
+mail_user="659900896@qq.com"    #用户名
+mail_pass="besideair23"   #口令 
+from getpass import getpass
+
+def prompt(prompt):
+    return input(prompt).strip()
+
+def sendMailToQQ(msg):
+    sender = '659900896@qq.com'
+    receivers = ['659900896@qq.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
+
+    message = MIMEText(msg, 'plain', 'utf-8')
+    message['From'] = Header("python脚本", 'utf-8')
+    message['To'] =  Header("qq邮箱", 'utf-8')
+     
+    subject = '电信地址改变了'
+    message['Subject'] = Header(subject, 'utf-8')
+    try:
+        smtpObj = smtplib.SMTP_SSL(mail_host) 
+        smtpObj.login( mail_user,mail_pass)  
+        smtpObj.sendmail(sender, receivers, message.as_string())
+        print "send mail success"
+    except smtplib.SMTPException:
+        print "Error: cannot send mail"
+
+
+
+class MyThread(Thread):
+    def __init__(self, event):
+        Thread.__init__(self)
+        self.stopped = event
+
+    def run(self):
+        while not self.stopped.wait(10):
+            print("my thread")
+            self.job()
+            # call a function
+    def job(self):
+        ip = get('https://api.ipify.org').text
+        print(ip)
+stopFlag = Event()
+thread = MyThread(stopFlag)
+thread.start()
+
+oldIp = get('https://api.ipify.org').text
+print('My public IP address is: {}'.format(oldIp))
+sendMailToQQ(oldIp)
+# this will stop the timer
+#stopFlag.set()
